209afc0ccf52802e85b3340b48c39fcc17e1793c
Jason Watson <j.watson@jitx.com>
2024-04-10 19:04:22 -0400

    conanfile.py: wrap bashisms in a bash wrapper for windows

--- conanfile.py
+++ conanfile.py
@@ -129,10 +129,10 @@ class ConanSlmPackage(ConanFile):
   # build(): Contains the build instructions to build a package from source
   def build(self):
     self.output.info("conanfile.py: build()")
-    self.run("pwd ; ls -la", cwd=self.source_folder, scope="build")
+    self.run("bash -c 'pwd ; ls -la'", cwd=self.source_folder, scope="build")
     self.run("stanza version", cwd=self.source_folder, scope="build")
     self.run("slm version", cwd=self.source_folder, scope="build")
-    self.run("[ ! -d .slm ] || slm clean", cwd=self.source_folder, scope="build")
+    self.run("bash -c '[ ! -d .slm ] || slm clean'", cwd=self.source_folder, scope="build")
     self.run("slm build -verbose -- -verbose", cwd=self.source_folder, scope="build")
 
     if not self.conf.get("tools.build:skip_test", default=False):
